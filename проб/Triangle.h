eede5d8ebc38c45bfcd4a25c1c80c2fc2775cf76
molyalya <mysovaoly10@gmail.com>
2025-05-11 21:52:48 +0300

    Update Triangle.h
    переведен

--- проб/Triangle.h
+++ проб/Triangle.h
@@ -6,25 +6,26 @@ using namespace std;
 template <class T>
 class Triangle
 {
+protected:
     T a, b, c;
 public:
     Triangle() : a(1), b(1), c(1)
     {
         if (!isValid())
-            throw invalid_argument("Error");
+            throw invalid_argument("Incorrect triangle arguments");
         if (!isComman())
-            throw invalid_argument("Error");
+            throw invalid_argument("Incorrect triangle arguments");
     }
 
     Triangle(T A, T B, T C) : a(A), b(B), c(C)
     {
-        if (!isValid()) throw invalid_argument("Error");
-        if (!isComman()) throw invalid_argument("Error");
+        if (!isValid()) throw invalid_argument("Incorrect triangle arguments");
+        if (!isComman()) throw invalid_argument("Incorrect triangle arguments");
     }
 
     ~Triangle() = default;
 
-        bool isValid() const
+    bool isValid() const
     {
         return (a > 0 && b > 0 && c > 0) && (a + b > c) && (a + c > b) && (b + c > a);
     }
@@ -40,21 +41,21 @@ public:
     {
         a = A;
         if (!isValid())
-            throw invalid_argument("Неверное значиние a");
+            throw invalid_argument("Invalid value for side a");
     }
 
     void set_b(T B)
     {
         b = B;
         if (!isValid())
-            throw invalid_argument("Неверное значиние b");
+            throw invalid_argument("Invalid value for side b");
     }
 
     void set_c(T C)
     {
         c = C;
         if (!isValid())
-            throw invalid_argument("Неверное значиние c");
+            throw invalid_argument("Invalid value for side c");
     }
     void set_sides(T A, T B, T C)
     {
@@ -62,7 +63,7 @@ public:
         b = B;
         c = C;
         if (!isValid())
-            throw invalid_argument("Неверное значиние всех сторон");
+            throw invalid_argument("Invalid values for all sides");
     }
 
     T get_a() const { return a; }
@@ -70,20 +71,20 @@ public:
     T get_c() const { return c; }
 
     void show() const {
-        cout << "Значения сторон: a = " << a << ", b = " << b << ", c = " << c << endl;
+        cout << "Side lengths: a = " << a << ", b = " << b << ", c = " << c << endl;
     }
 
     void change_sides() {
         int choice;
         T newSide, newSide1, newSide2, newSide3;
         while (true) {
-            cout << "\nМеню:\n"
-                << "1. Изменить сторону a\n"
-                << "2. Изменить сторону b\n"
-                << "3. Изменить сторону c\n"
-                << "4. Изменить все стороны\n"
-                << "5. Выйти из программы\n"
-                << "Выберите пункт: ";
+            cout << "\nMenu:\n"
+                << "1. Change side a\n"
+                << "2. Change side b\n"
+                << "3. Change side c\n"
+                << "4. Change all sides\n"
+                << "5. Exit\n"
+                << "Select option: ";
             cin >> choice;
 
             if (choice == 5) break;
@@ -91,31 +92,31 @@ public:
             try {
                 switch (choice) {
                 case 1:
-                    cout << "Введите новое значение а: ";
+                    cout << "Enter new value for a: ";
                     cin >> newSide;
                     set_a(newSide);
                     break;
                 case 2:
-                    cout << "Введите новое значение b: ";
+                    cout << "Enter new value for b: ";
                     cin >> newSide;
                     set_b(newSide);
                     break;
                 case 3:
-                    cout << "Введите новое значение c: ";
+                    cout << "Enter new value for c: ";
                     cin >> newSide;
                     set_c(newSide);
                     break;
                 case 4:
                     cin.ignore();
-                    cout << "Введите новые значения (пробел/entr): ";
+                    cout << "Enter new values (space/enter separated): ";
                     cin >> newSide1 >> newSide2 >> newSide3;
                     set_sides(newSide1, newSide2, newSide3);
                     break;
                 default:
-                    cout << "Выход из программы\n";
+                    cout << "Exiting program\n";
                     continue;
                 }
-                cout << "\nТекущие значения: \n";
+                cout << "\nCurrent values: \n";
                 show();
             }
             catch (const invalid_argument& e) {
